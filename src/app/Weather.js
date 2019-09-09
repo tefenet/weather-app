1d9cafb4e343296d9e18092ab2d1375a3266d7ea
tefenet <slobodo@protonmail.ch>
2019-09-09 14:36:46 -0300

    init

---
+++ src/app/Weather.js
@@ -0,0 +1,20 @@
+export class Weather {
+
+    constructor(city, countryCode) {
+        this.apikey = '23b2fab06e5ec988fb1ad8ea7103a2b9';
+        this.city = city;
+        this.countryCode = countryCode;
+    }
+
+    async getWeather() {
+        const URI = `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apikey}&units=metric`;
+        const response = await fetch(URI);
+        const data = await response.json();
+        return data;
+    }
+
+    changeLocation(city, countryCode) {
+        this.city = city;
+        this.countryCode = countryCode;
+    }
+}
